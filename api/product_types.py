1796e837ff490bab3851efbfdf7c3093a9e4f166
alltimenoob <mihirchauhan756@gmail.com>
2022-10-18 17:13:38 +0530

    Complete Products ProductTypes API ✅

--- api/product_types.py
+++ api/product_types.py
@@ -8,3 +8,22 @@ def getAll():
     query = "SELECT * FROM product_types"
     cursor = database.execute(query)
     return cursor.fetchall()
+
+def add(data):
+    try:
+        query = "INSERT INTO product_types(product_types_name) VALUES('%s')"%(data)
+        database.execute(query)
+        connection.commit()
+    except:
+        return -1
+    return 1
+
+def update(data):
+    try:
+        query = "UPDATE product_types set product_types_name = '%s' WHERE product_types_id = '%s'"%(data[1],data[0])
+        database.execute(query)
+        connection.commit()
+    except Exception as e:
+        print(e)
+        return -1
+    return 1
--- api/products.py
+++ api/products.py
@@ -1,11 +1,9 @@
 import sqlite3
 
 connection = sqlite3.connect('database.db')
-
-
+database = connection.cursor()
 
 def getAll():
-    database = connection.cursor()
     query = "SELECT * FROM products"
     cursor = database.execute(query)
     result = cursor.fetchall()
@@ -14,10 +12,28 @@ def getAll():
 
 
 def add(data):
-    database = connection.cursor()
-    query = "INSERT INTO products(products_name,quantity,price,products_types_id) \
-    VALUES('%s','%s','%s','%s')" % (data['products_name'] ,data['quantity'] \
-            ,data['price'] , data['products_types_id'][0])
-    cursor = database.execute(query)
-    connection.commit()
+    try:
+        query = "INSERT INTO products(products_name,quantity,price,products_types_id) \
+        VALUES('%s',%s,%s,%s)" % (data['products_name'] ,data['quantity'] \
+                ,data['price'] , data['products_types_id'])
+        print(query)
+
+        cursor = database.execute(query)
+        connection.commit()
+    except Exception as e:
+        print(e)
+        return -1
     return cursor.lastrowid
+
+def update(data):
+    print(data)
+    try:
+        query = "UPDATE products set products_name = '%s', quantity = '%s', price ='%s' \
+        , products_types_id = '%s' WHERE products_id = '%s'" % ( data['products_name'] ,\
+         data['quantity']  ,data['price'] , data['products_types_id'],data['product_id'])
+        database.execute(query)
+        connection.commit()
+    except:
+        return -1
+
+    return 1
